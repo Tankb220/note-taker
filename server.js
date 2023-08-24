2f3e309ce0d59d0b93e6aefd85a56a6e2aef265d
James Belcher <130000791+Tankb220@users.noreply.github.com>
2023-08-24 16:19:53 -0500

    made changes to server.js

--- server.js
+++ server.js
@@ -1,90 +1,46 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('uniqid'); 
 
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-let createNoteData = [];
-
+const app = express();
 
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// GET route for notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        // console.log(err);
+        res.json(JSON.parse(data));
+    })
+})
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        console.log(err);
+        const notes = JSON.parse(data);
+        req.body['uuid'] = uuid();
+        notes.push(req.body);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            console.log(err);
+            res.json(req.body);
+        })
+    })
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+// GET for index.html?
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-app.use(express.static(path.join(__dirname, "public")));
-
-
-app.get("/api/notes", function (err, res) {
-  try {
-    createNoteData = fs.readFileSync("db/db.json", "utf8");
-    console.log("Hello from the SERVER!");
-    createNoteData = JSON.parse(createNoteData);
-  } catch (err) {
-    console.log("\n error (catch err app.get):");
-    console.log(err);
-  }
-  res.json(createNoteData);
-});
-
-
-app.post("/api/notes", function (req, res) {
-  try {
-    createNoteData = fs.readFileSync("./db/db.json", "utf8");
-    console.log(createNoteData);
-    createNoteData = JSON.parse(createNoteData);
-    req.body.id = createNoteData.length;
-    createNoteData.push(req.body);
-    createNoteData = JSON.stringify(createNoteData);
-    fs.writeFile("./db/db.json", createNoteData, "utf8", function (err) {
-      if (err) throw err;
-    });
-
-    res.json(JSON.parse(createNoteData));
-  } catch (err) {
-    throw err;
-    console.error(err);
-  }
-});
-
-
-app.delete("/api/notes/:id", function (req, res) {
-  try {
-    createNoteData = fs.readFileSync("./db/db.json", "utf8");
-    createNoteData = JSON.parse(createNoteData);
-    createNoteData = createNoteData.filter(function (note) {
-      return note.id != req.params.id;
-    });
-    createNoteData = JSON.stringify(createNoteData);
-
-    fs.writeFile("./db/db.json", createNoteData, "utf8", function (err) {
-      if (err) throw err;
-    });
-
-    res.send(JSON.parse(createNoteData));
-  } catch (err) {
-    throw err;
-    console.log(err);
-  }
-});
-
-
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/api/notes", function (req, res) {
-  return res.sendFile(path.json(__dirname, "db/db.json"));
-});
-
-
-app.listen(PORT, function () {
-  console.log("SERVER IS LISTENING: " + PORT);
-});
